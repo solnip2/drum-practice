a1b302d45e8802bfa3e484d2e542ef54c8b274d5
solnip2 <solnip2@gmail.com>
2026-07-01 16:15:14 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "pad-drum-practice-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./drum-practice.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
